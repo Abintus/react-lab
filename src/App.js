7fe2e325c2c4a3e30673d9dcb3a58029294b3060
Abintus <samelalukasz@gmail.com>
2025-01-12 10:55:36 +0100

    Added reactivity to page components.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
-function handleChange(event){
-    console.log(event.target.value);
-}
+import {useState} from "react";
 
 function App() {
-    let title = "Wall-E"
+    const [title, setTitle] = useState("Wall-E");
+    let message = "";
+    if (title.length < 5) {
+        message="Isn't it a bit too short?";
+    } else if (title.length < 15) {
+        message = "Purrfect! Ideal title length!"
+    } else {
+        message="What are you trying to do? Write an essay?";
+        }
+  function handleChange(event){
+    setTitle(event.target.value);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
         My favorite movies to watch
       </header>
       <h2> My favorite movie for today is {title} </h2>
-    <input type="text" onChange={handleChange}/>
+      {title.length > 0 && <div> {message} </div>}
+    <input type="text" onChange={handleChange} value={title}/>
 
     </div>
   );
